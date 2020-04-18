3f7ff9e31c89b5d813d925277a349c29f4b6cb7e
Marek Fořt <marek@me.com>
2020-04-18 12:00:20 +0200

    Add comments.

--- Sources/DynamicArgumentParser/Scaffold.swift
+++ Sources/DynamicArgumentParser/Scaffold.swift
@@ -10,9 +10,13 @@ struct Scaffold: ParsableCommand {
     }
     
     static func preprocess(_ arguments: [String]) throws {
+        // Obtaining template name
         let templateName = arguments[1]
+        // Based on template name find its manifest
         let manifestPath = FileManager.default.currentDirectoryPath + "/\(templateName)/manifest.json"
+        // Obtain data
         let data = try Data(contentsOf: URL(fileURLWithPath: manifestPath))
+        // Parse the attributes
         let attributes: [String] = try JSONDecoder().decode([String].self, from: data)
         print(attributes)
     }
